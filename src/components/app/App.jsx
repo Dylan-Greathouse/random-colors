bb1bc7de9592171dfe0ca7e74bea978fbc7c376f
Dylan-Greathouse <Greathouse.dylanj@gmail.com>
2021-11-05 11:06:07 -0700

    pushing up changes, got random color to render in the box

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,13 +1,28 @@
-import React from 'react';
-import RandomColor from './RandomColor';
+import React, { Component } from 'react';
+import { RandomColor } from './RandomColor';
+import '.././App.css';
 
 
-export default function App() {
-  return(
-    <>
-    <h1>HELLO WORLD</h1>
-    <RandomColor />
-    </>
-  );
+export default class App extends Component {
+ state = { color: 'blue' };
 
+ componentDidMount() {
+   this.timerID = setInterval(
+     () => this.setState({ color: this.getRandomColor() }),
+      1000
+   );
+ }
+
+ componentDidUnmount() {
+   clearInterval(this.timerID);
+ }
+
+ getRandomColor() {
+  const rand255 = () => Math.floor(Math.random() * 256);
+  return `rgb(${rand255()}, ${rand255()}, ${rand255()})`;
+}
+
+render() {
+  return <RandomColor color={this.state.color} />;
+}
 }
--- src/components/app/RandomColor.jsx
+++ src/components/app/RandomColor.jsx
@@ -1,32 +1,30 @@
 import React, { Component } from 'react';
-import ColorDisplay from './ColorDisplay';
-import ColorButton from './ColorButton';
-
-function getRandomColor(color) {
-    return Math.ceil(Math.random() * color);
-  };
-class RandomColor extends Component {
-    state = {
-        currentColor: '',
-    };
+import '.././App.css';
 
 
-    handleColorChange = (color) => {
-    this.setState({ currentColor: color });
-    };
-
+export class RandomColor extends Component {
     render() {
-        const colors = ['blue', 'red', 'yellow', 'green', 'pink', 'teal', 'white', 'purple'];
-        const { currentColor } = getRandomColor (colors);
-
-    return (
-        <>
-         <h1>Press Button for Random Colors!</h1>
-        <ColorButton />
-        <ColorDisplay currentColor={currentColor} />
-        </>
-
-    );
+        const { color } = this.props;
+        return (
+            <div
+                className="colorBox"
+                style={
+                    this.props.color[4] !== '9'
+                    ? {
+                          width: '500px',
+                          height: '500px',
+                          backgroundColor: color,
+                      }
+                    : {
+                          width: '500px',
+                          height: '500px',
+                          
+                      }
+                }
+                
+            
+            ></div>
+        );
     }
 }
 export default RandomColor;
